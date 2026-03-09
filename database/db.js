f926968aad062f7279495d4aed3ecde5cfe8b389
mike_MSI <later481@gmail.com>
2026-03-08 19:21:13 -0600

    feat: connection to database

---
+++ database/db.js
@@ -0,0 +1,23 @@
+const sql = require('mssql')
+
+const config = {
+    user: 'sa',
+    password: 'mortal_12',
+    server: '192.168.100.20',
+    database: 'InventarioTI',
+    options: {
+        encrypt: false,
+        trustServerCertificate: true
+    }
+}
+
+async function connectDB() {
+    try {
+        await sql.connect(config)
+        console.log("Conexion exitosa a SQL Server")
+    } catch (err) {
+        console.log("Error de conexion:", err)
+    }
+}
+
+module.exports = { connectDB }
